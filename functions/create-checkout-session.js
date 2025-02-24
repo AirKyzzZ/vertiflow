cc3d347b00e25fc9dc1896fa7f63416454e9060b
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-02-24 23:55:18 +0100

    hosting_directory_public

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const EmailJS = require('@emailjs/browser');
 
 exports.handler = async (event) => {
-    // ... [votre code existant] ...
+    // testing public
 
     // Envoi d'email avec EmailJS
     await EmailJS.send(
