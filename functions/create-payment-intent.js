310121a1bd9223cea9918716a4941d7acd4d3c1b
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-02-25 00:38:42 +0100

    fix_2

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,23 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+    try {
+        const { amount } = JSON.parse(event.body);
+        
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'eur',
+            automatic_payment_methods: { enabled: true },
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
