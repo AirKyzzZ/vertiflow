2a259a3875b475a2123eb8097c72b9e93acad0d2
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-02-25 09:17:35 +0100

    test_stripe_pay

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,5 +1,5 @@
 // create-payment-intent.js
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST_KEY);
 
 exports.handler = async (event) => {
   try {
