0729fdd1dd2f5d6887d1d9b37420e3672f82c84c
AirKyzzZ <AirKyzzZ@users.noreply.github.com>
2026-08-22 21:36:08 +0200

    feat: mdx foundation and prose component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  pageExtensions: ['ts', 'tsx', 'mdx'],
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'files.cdn.printful.com' }],
   },
-};
+}
 
-export default nextConfig;
+export default createMDX({})(nextConfig)
